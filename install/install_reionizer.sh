b899b0fb124a81e62044ba8c9279b351962fa496
Charlotte Mason <charlotte.nosam@gmail.com>
2022-06-13 16:55:13 +0200

    Update for new project group

--- install/install_reionizer.sh
+++ install/install_reionizer.sh
@@ -6,17 +6,17 @@ module load anaconda3/2021.11
 conda init bash
 
 # create lya_env environment in project directory
-conda create --prefix /home/projects/ku_00154/apps/.conda/envs/lya_env python=3.7
+conda create --prefix /home/projects/ku_00155/apps/modulefiles/.conda/envs/lya_env python=3.7
 conda activate lya_env
 
 # install requirements
 conda install pip
 
 # Make sure the code can find the data
-export LYA_DATA_DIR=/home/projects/ku_00154/data/LyaInference_data/
+export LYA_DATA_DIR=/home/projects/ku_00155/data/LyaInference_data/
 
 # Afterwards we clone and install the LyaInference package itself
-cd /home/projects/ku_00154/apps/
+cd /home/projects/ku_00155/apps/
 git clone https://github.com/charlottenosam/LyaInference.git
 cd LyaInference
 python setup.py install
